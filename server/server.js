ed182fefe47bbf99dcf3f67b849c170d2c33ba6d
duvarakeshss <duvarakesh05@gmail.com>
2025-05-07 20:19:38 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/register', registerRoutes);
+app.use('/api/register', registerRoutes);
 app.use('/api/recognize', recognitionRoutes);
 
 app.get('/', (req, res) => {
