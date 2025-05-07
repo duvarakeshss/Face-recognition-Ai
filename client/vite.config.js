f350d4481f5a317373bd012fe26f2efbec056921
duvarakeshss <duvarakesh05@gmail.com>
2025-05-07 19:18:27 +0530

    commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: "https://face-server.duvarakesh.xyz",
+        target: apiProxyUrl,
         changeOrigin: true
       }
     }
