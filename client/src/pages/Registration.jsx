cbbe7c7b945e560771d73d6fce4aca90b58d6845
duvarakeshss <duvarakesh05@gmail.com>
2025-05-07 20:50:37 +0530

    commit

--- client/src/pages/Registration.jsx
+++ client/src/pages/Registration.jsx
@@ -10,7 +10,7 @@ function Registration() {
   const [registrationResult, setRegistrationResult] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   // API URL can be accessed from the environment variable
-  const baseUrl = import.meta.env.VITE_API_URL||'http://face-server.duvarakesh.xyz';
+  const baseUrl = import.meta.env.VITE_API_URL||'https://face-server.duvarakesh.xyz';
   console.log('VITE_API_URL:', baseUrl);
   const apiUrl = `${baseUrl}/api/register`;
 
