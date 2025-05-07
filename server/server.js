c6f1d24415e6a18cdb62ac64119d8e4c08a1e8ff
duvarakeshss <duvarakesh05@gmail.com>
2025-05-07 20:38:27 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -10,9 +10,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
-app.use(express.json());
-
+app.use(express.json({ limit: "100mb" }));
+app.use(
+  cors({
+    origin: ["https://face-recognition-ai.vercel.app","http://localhost:5173"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 // Routes
 app.use('/api/register', registerRoutes);
 app.use('/api/recognize', recognitionRoutes);
